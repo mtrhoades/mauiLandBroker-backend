b685e470d1ac2a43099eac6278b251988139ce51
root <mtrhoades0727@gmail.com>
2023-09-08 12:24:27 -1000

    server setup

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// dependencies
+const express = require('express');
+const cors = require('cors');
+
+// configuration
+require('dotenv').config();
+const PORT = process.env.PORT;
+const app = express();
+
+// middleware
+app.set('views', __dirname + '/views');
+app.use(express.static('public')); // access to public folder for css and images.
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(cors());
+app.use(express.json());
+
+// root route (home page for backend portal)
+app.get('/', (req, res) => {
+    res.render("backendHomePage");
+});
+
+// controller routes here
+
+// server listen
+app.listen(PORT, () => {
+    console.log("Yea Dawg we up in here on Port", PORT)
+});
+
---
+++ src/views/backendHomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const backendHomePage = () => {
+  return (
+    <div>
+        <h1>Landing Page for backend Portal</h1>
+
+    </div>
+  )
+}
+
+export default backendHomePage;
