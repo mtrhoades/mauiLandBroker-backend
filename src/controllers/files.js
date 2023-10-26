8dc535a10b248a3c4e2fd09d397cd0477d574819
root <mtrhoades0727@gmail.com>
2023-10-26 10:25:19 -1000

    post route works for files controller to add new category object by category name input

--- src/controllers/files.js
+++ src/controllers/files.js
@@ -17,31 +17,17 @@ files.get('/:id', async (req, res) => {
     }
 });
 
-// GET ALL route for file categories to specific association
-// files.get('/:id', async (req, res) => {
-//     const associationId = req.params.id;
-//     try {
-//         const result = await Association.find();
-//         // res.json({"associations": result});
-//         res.render("fileCategorySingleAssoc", {
-//             association: result,
-//         });                
-//     } catch (error) {
-//         res.status(500).json({error: error.message});
-//     }
-// });
-
 // POST route for adding a file category
 files.post('/:id', async (req, res) => {
     const associationId = req.params.id;
     const newCategory = {
-        categoryname: 'New Category Name', // needs to link to whatever is typed in input field!
+        categoryname: '',
         files: []
       };
     try {
         await Association.findOneAndUpdate(
             {_id: associationId},
-            {$push: {filecategories: newCategory}},
+            {$push: {'filecategories': newCategory.categoryname = req.body}},
             {new: true}
         );
         res.redirect(req.get('referer'));
